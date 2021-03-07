71f9506ff6dc00d961eb2c618b8563bc92b0a3e7
alyssahellrung <alyssahellrung@gmail.com>
2021-03-07 15:51:05 -0800

    started data3.js

---
+++ data3.js
@@ -0,0 +1,91 @@
+const questions = [
+  {
+    "question": "Monday",
+    "answer": "lundi" 
+  },
+  {
+    "question": "Tuesday",
+    "answer": "mardi" 
+  },
+  {
+    "question": "Wednesday",
+    "answer": "mercredi" 
+  },
+  {
+    "question": "Thursday",
+    "answer": "jeudi" 
+  },
+  {
+    "question": "Friday",
+    "answer": "vendredi" 
+  },
+  {
+    "question": "Saturday",
+    "answer": "samedi" 
+  },
+  {
+    "question": "Sunday",
+    "answer": "dimanche" 
+  },
+  {
+    "question": "week",
+    "answer": "la semaine" 
+  },
+  {
+    "question": "month",
+    "answer": "le mois" 
+  },
+  {
+    "question": "year",
+    "answer": "l'annee" 
+  },
+  {
+    "question": "January",
+    "answer": "janvier" 
+  },
+  {
+    "question": "February",
+    "answer": "fevrier" 
+  },
+  {
+    "question": "March",
+    "answer": "mars" 
+  },
+  {
+    "question": "April",
+    "answer": "avril" 
+  },
+  {
+    "question": "May",
+    "answer": "mai" 
+  },
+  {
+    "question": "June",
+    "answer": "juin" 
+  },
+  {
+    "question": "July",
+    "answer": "julliet" 
+  },
+  {
+    "question": "August",
+    "answer": "aout" 
+  },
+  {
+    "question": "September",
+    "answer": "septembre" 
+  },
+  {
+    "question": "October",
+    "answer": "octobre" 
+  },
+  {
+    "question": "November",
+    "answer": "novembre" 
+  },
+  {
+    "question": "December",
+    "answer": "decembre" 
+  },
+  
+]
